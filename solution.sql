ca22ab579e2c4507167f1749a1da41fb225e7b4f
SARAVANAKUMAR A K <saravanakumar147sk@gmail.com>
2026-08-21 10:04:29 +0530

    Program 1

--- solution.sql
+++ solution.sql
@@ -3,7 +3,7 @@ CREATE DATABASE CollegeDB;
 USE CollegeDB;
 
 CREATE TABLE Department (
-    DepartmentID INT(5) PRIMARY KEY
+    DepartmentID INT(5) PRIMARY KEY,
     DepartmentName VARCHAR(20),
     HOD VARCHAR (20)
 );
